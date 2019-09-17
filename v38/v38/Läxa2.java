7183939dbc338c0ed12d71c7cca3350878355d5b
NinjaGrandpa <max@jenslov.se>
2019-09-17 20:20:57 +0200

    Skriver ut största, minsta och medelvärdstal.

--- v38/v38/Läxa2.java
+++ v38/v38/Läxa2.java
@@ -8,56 +8,68 @@ public class Läxa2 {
 		
 		Scanner input1 = new Scanner(System.in);
 		
-		int storstatal1 = 0;
-		int minstatal1 = 0;
+		float storstatal1 = 0f;
+		float minstatal1 = 0f;
 		
 		System.out.println("Skriv in 5 heltal: ");
-		int heltal1 = input1.nextInt();
-		int heltal2 = input1.nextInt();
-		int heltal3 = input1.nextInt();
-		int heltal4 = input1.nextInt();
-		int heltal5 = input1.nextInt();
+		float heltal1 = input1.nextFloat();
+		float heltal2 = input1.nextFloat();
+		float heltal3 = input1.nextFloat();
+		float heltal4 = input1.nextFloat();
+		float heltal5 = input1.nextFloat();
 		
-		if (heltal1 > heltal2 && heltal1 > heltal3 && 
-			heltal1 > heltal4 && heltal1 > heltal5) {
+		if (heltal1 >= heltal2 && heltal1 >= heltal3 && 
+			heltal1 >= heltal4 && heltal1 >= heltal5) {
 			storstatal1 = heltal1;	
 		}
-		if (heltal1 < heltal2 && heltal1 < heltal3 &&
-			heltal1 < heltal4 && heltal1 < heltal5) {
+		if (heltal1 <= heltal2 && heltal1 <= heltal3 &&
+			heltal1 <= heltal4 && heltal1 <= heltal5) {
 			minstatal1 = heltal1;
 		}
-		if (heltal2 > heltal1 && heltal2 > heltal3 && 
-			heltal2 > heltal4 && heltal2 > heltal5) {
+		if (heltal2 >= heltal1 && heltal2 >= heltal3 && 
+			heltal2 >= heltal4 && heltal2 >= heltal5) {
 			storstatal1 = heltal2;
 		}
-		if (heltal2 < heltal1 && heltal2 < heltal3 &&
-			heltal2 < heltal4 && heltal2 < heltal5) {
+		if (heltal2 <= heltal1 && heltal2 <= heltal3 &&
+			heltal2 <= heltal4 && heltal2 <= heltal5) {
 			minstatal1 = heltal2;
 		}
-		if (heltal3 > heltal1 && heltal3 > heltal2 && 
-			heltal3 > heltal4 && heltal3 > heltal5) {
+		if (heltal3 >= heltal1 && heltal3 >= heltal2 && 
+			heltal3 >= heltal4 && heltal3 >= heltal5) {
 			storstatal1 = heltal3;	
 		}
-		if (heltal3 < heltal1 && heltal3 < heltal2 && 
-			heltal3 < heltal4 && heltal3 < heltal5) {
+		if (heltal3 <= heltal1 && heltal3 <= heltal2 && 
+			heltal3 <= heltal4 && heltal3 <= heltal5) {
 			minstatal1 = heltal3;	
 		}
-		if (heltal4 > heltal1 && heltal4 > heltal2 && 
-			heltal4 > heltal3 && heltal4 > heltal5) {
+		if (heltal4 >= heltal1 && heltal4 >= heltal2 && 
+			heltal4 >= heltal3 && heltal4 >= heltal5) {
 			storstatal1 = heltal4;	
 		}
-		if (heltal4 < heltal1 && heltal4 < heltal2 && 
-			heltal4 < heltal3 && heltal4 < heltal5) {
+		if (heltal4 <= heltal1 && heltal4 <= heltal2 && 
+			heltal4 <= heltal3 && heltal4 <= heltal5) {
 			minstatal1 = heltal4;	
 		}
-		if (heltal5 > heltal1 && heltal5 > heltal2 && 
-			heltal5 > heltal3 && heltal5 > heltal4) {
+		if (heltal5 >= heltal1 && heltal5 >= heltal2 && 
+			heltal5 >= heltal3 && heltal5 >= heltal4) {
 			storstatal1 = heltal5;	
 		}
-		if (heltal5 < heltal1 && heltal5 < heltal2 && 
-			heltal5 < heltal3 && heltal5 < heltal4) {
+		if (heltal5 <= heltal1 && heltal5 <= heltal2 && 
+			heltal5 <= heltal3 && heltal5 <= heltal4) {
 			minstatal1 = heltal5;	
+		}
+		
+		int intStorstatal1 = (int) storstatal1;
+		int intMinstatal1 = (int) minstatal1;
+		
+		float medelvarde1 = ((heltal1 + heltal2 + heltal3 + 
+				heltal4 + heltal5)/5);
+		
+		
+		System.out.println("Högst: " + intStorstatal1 + "\nLägst: "
+				+ intMinstatal1 + "\nMedel: " + medelvarde1);
+			
 		}
 
 	}
-}
+
