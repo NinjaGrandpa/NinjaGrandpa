ba598c7666726c7368f8e08f3b265b8982f3a5cf
NinjaGrandpa <max@jenslov.se>
2019-09-24 11:56:13 +0200

    Skriver ett definerat mönster med hjälp av loopar

---
+++ v39/v39/uppgift7.java
@@ -0,0 +1,14 @@
+package v39;
+
+public class uppgift7 {
+
+	public static void main(String[] args) {
+		
+		String X = ("x");
+		for (int i = 0; i < 5; i++ ) {
+			System.out.println(X);
+			X = X + ("x");
+		}
+		
+	}
+}
