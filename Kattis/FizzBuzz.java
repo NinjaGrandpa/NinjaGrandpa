47984d09f4c416824c2d8590432824c1ee1fbf80
NinjaGrandpa <max@jenslov.se>
2019-10-03 08:46:43 +0200

    Kattis Stones+ läxa

---
+++ Kattis/FizzBuzz.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int x=input.nextInt();
+		int y=input.nextInt();
+		int n=input.nextInt();
+		
+		for (int i = 1; i < (n + 1); i++ ) {
+			
+			if (i % x == 0 && i % y == 0) {
+				System.out.println("FizzBuzz");
+			}
+			
+			else if (i % x == 0) {
+				System.out.println("Fizz");
+			}
+			else if (i % y == 0) {
+				System.out.println("Buzz");
+			}
+			
+			else {
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
